a1a0823951219f08d4afcb45b31d430f35306837
Guilherme Souza <sitegui@sitegui.com.br>
2014-12-29 03:04:54 -0200

    Using tls

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var fs = require('fs')
+
 module.exports = {
 	// Capped collection size (in bytes)
 	logSize: 1024 * 1024 * 1024,
@@ -7,6 +9,9 @@ module.exports = {
 	mongoUri: 'mongodb://localhost:27017/logSink',
 	// Socket options (will be passed to net.createServer or tls.createServer)
 	socket: {
+		// DO NOT USE THE EXAMPLE KEY/CERT since they were made public, ok?
+		key: fs.readFileSync('./keys/example-key.pem'),
+		cert: fs.readFileSync('./keys/example-cert.pem')
 	},
 	// Port to bind to
 	port: 8018
